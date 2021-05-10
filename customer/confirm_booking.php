672ae24d130b3cba161dc0a837a6d12c1ebe4096
PENG GAO <gaopeng19931027@gmail.com>
2021-05-10 17:52:13 +1000

    change

--- customer/confirm_booking.php
+++ customer/confirm_booking.php
@@ -23,29 +23,6 @@ if (isset($_GET['Price'])) {
     $house_price = $_GET['Price'];
 }
 
-// if (isset($_GET['id'])) {
-//     // include "../../db_conn.php";
-
-//     function validate($data)
-//     {
-//         $data = trim($data);
-//         $data = stripslashes($data);
-//         $data = htmlspecialchars($data);
-//         return $data;
-//     }
-
-//     $id = validate($_GET['id']);
-
-//     $sql = "SELECT * FROM house WHERE id=$id";
-//     $result = mysqli_query($db, $sql);
-//     if (mysqli_num_rows($result) > 0) {
-//         $row = mysqli_fetch_assoc($result);
-//     } else {
-//         // echo ("ok");
-//         header("Location: ../login_book.php");
-//     }
-// }
-
 ?>
 
 
